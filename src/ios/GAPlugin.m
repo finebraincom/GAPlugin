d7e9b1ecb8e6cce06080555417d44f6a84677ff2
Iurii Kyian <iuriikyian@Fines-MacBook-Pro-5.local>
2014-12-04 15:43:22 +0100

    code corrected to work with Phonegap 4.0

--- src/ios/GAPlugin.m
+++ src/ios/GAPlugin.m
@@ -91,7 +91,7 @@
 {
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
 
-    [self writeJavascript:[commandResult toSuccessCallbackString:callbackID]];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
 }
 
 -(void)failWithMessage:(NSString *)message toID:(NSString *)callbackID withError:(NSError *)error
@@ -99,7 +99,7 @@
     NSString        *errorMessage = (error) ? [NSString stringWithFormat:@"%@ - %@", message, [error localizedDescription]] : message;
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
 
-    [self writeJavascript:[commandResult toErrorCallbackString:callbackID]];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
 }
 
 -(void)dealloc
